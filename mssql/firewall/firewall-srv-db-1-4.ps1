3c5536d7c7d40b1338c22208e428b080955b6d3e
Евдокимов А.А <unk1nd@triumf.local>
2022-01-14 11:41:48 +0600

    working hard

--- mssql/firewall/firewall-srv-db-1-4.ps1
+++ mssql/firewall/firewall-srv-db-1-4.ps1
@@ -11,9 +11,10 @@ New-NetFirewallRule -DisplayName "ALLOW TCP PORT 1025 lsass.exe" -Direction inbo
 New-NetFirewallRule -DisplayName "ALLOW TCP PORT 1311 rxmon" -Direction inbound -Profile Any -Action Allow -LocalPort 1311 -Protocol TCP
 New-NetFirewallRule -DisplayName "ALLOW TCP PORT 1433 msSQL" -Direction inbound -Profile Any -Action Allow -LocalPort 1433 -Protocol TCP
 New-NetFirewallRule -DisplayName "ALLOW TCP PORT 1434 msSQL Discovery Service" -Direction inbound -Profile Any -Action Allow -LocalPort 1434 -Protocol TCP
-New-NetFirewallRule -DisplayName "ALLOW UDP PORT 1541, 1560-1591 1cv83" -Direction inbound -Profile Any -Action Allow -LocalPort 1541, 1560-1591 -Protocol UDP
-New-NetFirewallRule -DisplayName "ALLOW TCP PORT 1541, 1560-1591 1cv83" -Direction inbound -Profile Any -Action Allow -LocalPort 1541, 1560-1591 -Protocol TCP
+New-NetFirewallRule -DisplayName "ALLOW UDP PORT 1540, 1541, 1560-1591 1cv83" -Direction inbound -Profile Any -Action Allow -LocalPort 1540, 1541, 1560-1591 -Protocol UDP
+New-NetFirewallRule -DisplayName "ALLOW TCP PORT 1540, 1541, 1560-1591 1cv83" -Direction inbound -Profile Any -Action Allow -LocalPort 1540, 1541, 1560-1591 -Protocol TCP
 New-NetFirewallRule -DisplayName "ALLOW TCP PORT 1947 sentinelsrm" -Direction inbound -Profile Any -Action Allow -LocalPort 1947 -Protocol TCP
 New-NetFirewallRule -DisplayName "ALLOW TCP PORT 3389, 3390 RDP" -Direction inbound -Profile Any -Action Allow -LocalPort 3389,3390 -Protocol TCP
 New-NetFirewallRule -DisplayName "ALLOW UDP PORT 3389, 3390 RDP" -Direction inbound -Profile Any -Action Allow -LocalPort 3389,3390 -Protocol UDP
+New-NetFirewallRule -DisplayName "ALLOW UDP PORT 5355 Multicast name Discovery" -Direction inbound -Profile Any -Action Allow -LocalPort 5355 -Protocol UDP
 New-NetFirewallRule -DisplayName "ALLOW TCP PORT 5357 WSDapi (webserices)" -Direction inbound -Profile Any -Action Allow -LocalPort 5357 -Protocol TCP
